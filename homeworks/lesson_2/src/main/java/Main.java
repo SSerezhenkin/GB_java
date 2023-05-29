ea18034256ecb23a2f99d5c0815571914382ba27
Сергей Серёженькин <sv.serezhenkin@gmail.com>
2023-05-29 22:31:50 +0300

    finish first

--- homeworks/lesson_2/src/main/java/Main.java
+++ homeworks/lesson_2/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
-
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        JSONObject filter = new JSONObject(json);
+        StringBuilder whereClause = new StringBuilder();
+        for (String key : filter.keySet()) {
+            String value = filter.getString(key);
+            if (value != null && !value.equals("null")) {
+                whereClause.append(key).append("='").append(value).append("' AND ");
+            }
+        }
+        String whereClauseStr = whereClause.substring(0, whereClause.length() - 5);
+        String sql = "SELECT * FROM students WHERE " + whereClauseStr;
     }
 }
